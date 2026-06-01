44019577c38e9607f0bea3503d1a683c49649aa8
Farfaras <94570343+Farfaras@users.noreply.github.com>
2026-06-01 00:20:18 -0400

    archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/{any}', function () {
-    return view('welcome');
+
+    $path = public_path('index.html');
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    return Response::file($path);
+
 })->where('any', '.*');
