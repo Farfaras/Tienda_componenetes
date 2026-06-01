2ae3abb20ddf7112c0d5b09454cdeaf60a00ea2a
ccastedo19 <cesar.castedo1@gmail.com>
2026-05-31 23:24:47 -0400

    correcion prueba 2 route

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'status' => 'ok',
-        'message' => 'Laravel funcionando'
-    ]);
+    return "Frontend funcionando";
 });
