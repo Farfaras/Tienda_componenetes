9cd0ebab4753639b439c5c87c228103ac9e5bd34
Farfaras <94570343+Farfaras@users.noreply.github.com>
2026-06-02 07:44:23 -0400

    configuracion cors

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    // 'allowed_origins' => ['http://localhost:5173/'],
+    'allowed_origins' => ['https://tienda-componentes-frontend.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
